be195f5077208bb7d31fe32f44420abf2c453b24
JoaoFelipin <joaoed.felipin@gmail.com>
2024-01-23 19:11:17 -0300

    aula de exemplo streamlit

--- examples/streamlit_1.py
+++ examples/streamlit_1.py
@@ -4,3 +4,14 @@ st.set_page_config(page_title='Aula DNC')
 
 st.write('Hello')
 
+st.header('Titulo da página')
+st.markdown('Aqui um texto')
+
+st.sidebar.header('texto da sidebar')
+
+nome = st.text_input('Nome')
+st.write(f'ola {nome} ')
+
+cargo = ['Engenheiro','Programador','Data Scientist']
+select_cargo = st.sidebar.selectbox('Qual seu cargo?',options=cargo)
+st.sidebar.write(select_cargo)
