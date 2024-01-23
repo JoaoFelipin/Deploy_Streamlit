4a8622ac5561a422a70917fb9430d647078c15d8
JoaoFelipin <joaoed.felipin@gmail.com>
2024-01-23 19:39:47 -0300

    wip

--- app/app2.py
+++ app/app2.py
@@ -1,13 +1,13 @@
-import pickle as pk 
+import pickle
 import streamlit as st 
 import pandas as pd 
 
 
 st.set_page_config(page_title='Deploy do modelo de insurance')
+st.title('Insurance Prediction')
 
 #parametros
 idade = st.number_input(label='Age',value=18,min_value=18,max_value=120)
 bmi = st.number_input(label='BMI',value=30.0)
 children = st.slider(label='Children',min_value=0)
 smoker = st.selectbox(label='Smoker',options=['No','Yes'])
-
